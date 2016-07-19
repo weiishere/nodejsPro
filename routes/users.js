fb9acfe5c3529a413ba7dd5de0a32b8456ee4508
黄伟 <wei.huang@hotmail.com>
2016-07-20 01:29:02 +0800

    go
    Signed-off-by: 黄伟 <wei.huang@hotmail.com>

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,11 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+  next();
 });
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'login' });
+});
+
 
 module.exports = router;
