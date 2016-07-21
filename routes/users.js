38eee4a18053966e202fdf8b1956890b00c6fed1
huangwei <huangwei10@jd.com>
2016-07-21 21:23:28 +0800

    Signed-off-by: huangwei <huangwei10@jd.com>

--- routes/users.js
+++ routes/users.js
@@ -2,13 +2,25 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
   next();
 });
-router.get('/login', function(req, res, next) {
+router.get('/login', function (req, res, next) {
   res.render('login', { title: 'login' });
+  //next();
 });
 
+router.post('/dologin', function (req, res, next) {
+  var user = {
+    username: 'admin',
+    password: 'admin'
+  }
+  if (req.body.username === user.username && req.body.password === user.password) {
+    res.redirect('/home2');
+  }else{
+    res.redirect('login');
+  }
+});
 
 module.exports = router;
